1b38c7600bac74b12dba059ae69513da45874572
Daniel Hoe <danielhoe97@gmail.com>
2019-06-08 03:11:32 +0800

    Updated graphics
    Added logo, added font

--- Assets/Scripts/MarcoPoloGame.cs
+++ Assets/Scripts/MarcoPoloGame.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class MarcoPoloGame {
     public const int ROUNDS_PER_GAME = 4;
-    public const int PLAYERS_IN_MATCH = 1;
-    public const int ROUND_TIME = 3;
+    public const int PLAYERS_IN_MATCH = 2;
+    public const int ROUND_TIME = 90;
 
-    public const int PRE_ROUND_TIME = 3;
+    public const int PRE_ROUND_TIME = 10;
 
     public const string IS_HUNTER = "isHunter";
     public const string IS_ALIVE = "isAlive";
