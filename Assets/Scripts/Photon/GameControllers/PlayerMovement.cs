986887c47a8bde7f9eb6553f9da0db08c880d207
Yeo Tong <helloimyeotong@gmail.com>
2019-06-19 12:31:31 +0800

    Fixed error for null object reference
    Modified code from OnCollisionEnter2D to OnCollisionStay2D in PlayerMovement.cs

--- Assets/Scripts/Photon/GameControllers/PlayerMovement.cs
+++ Assets/Scripts/Photon/GameControllers/PlayerMovement.cs
@@ -111,7 +111,7 @@ public class PlayerMovement : MonoBehaviour
         transform.Rotate(new Vector3(0, mouseX, 0));
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D (Collision2D collision)
     {
         if(collision.gameObject.GetPhotonView() != null) {
             bool isOtherHunter = (bool) collision.gameObject
